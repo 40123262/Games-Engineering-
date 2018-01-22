9f10a023ee40ab745bd9b475d609d2b6e43dacaa
Jan Mencfel <40123262@live.napier.ac.uk>
2018-01-22 01:48:14 +0000

    Ship header , cpp and game header created

--- PRACTICAl_2/main.cpp
+++ PRACTICAl_2/main.cpp
@@ -1,4 +1,7 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "ship.h"
+#include "Game.h"
 
 using namespace sf;
 using namespace std;
@@ -8,10 +11,18 @@ const Keyboard::Key controls[4] = {
 	Keyboard::Right,
 	Keyboard::Space
 };
-
+vector<Ship *> ships;
+Texture spritesheet;
+Sprite invader;
 void Load()
 {
-	
+	if(!spritesheet.loadFromFile("res/img/invaders_sheet.png"))
+	{
+		cerr << "Failed to load spritesheet!" << endl;
+	}
+	invader.setTexture(spritesheet);
+	invader.setTextureRect(IntRect(0, 0, 32, 32));
+
 }
 void reset()
 {
@@ -19,19 +30,41 @@ void reset()
 }
 void Update(RenderWindow &window)
 {
-	
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+
+	Event event;
+	while (window.pollEvent(event))
+	{
+		if (event.type == Event::Closed)
+		{
+			window.close();
+			return;
+		}
+	}
+
+	if (Keyboard::isKeyPressed(Keyboard::Escape))
+	{
+		window.close();
+	}
 }
 
 void Render(RenderWindow &window)
 {
-	
+	window.draw(invader);
 }
 
 int main()
 {
-	
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "Space Invaders");
 	Load();
 	reset();
-	
+	while (window.isOpen())
+	{
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
 	return 0;
 }
